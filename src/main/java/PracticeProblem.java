c6e0e34e83a3d8a9cef79dc6165008f9a5b4c755
franklin-zhu-ics4u1-02-25 <123124410+FranklinZhu1@users.noreply.github.com>
2025-04-11 03:32:52 +0000

    Create solution methods

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,77 @@
+import java.io.*;
+import java.util.*;
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-		
+	public static String getName(int line, String file) {
+		Scanner in = null;
+		try {
+			if (line < 1) return "";
+			in = new Scanner(new BufferedReader(new FileReader(file)));
+			for (int i = 0; i < line - 1; ++i) in.nextLine();
+			return in.next() + " " + in.next();
+		}
+		catch (Exception e) {
+			return "";
+		}
+		finally {
+			if (in != null) in.close();
+		}
+	}
+
+	public static int getAge(int line, String file) {
+		Scanner in = null;
+		try {
+			if (line < 1) return -1;
+			in = new Scanner(new BufferedReader(new FileReader(file)));
+			for (int i = 0; i < line - 1; ++i) in.nextLine();
+			in.next(); // skip the first name
+			in.next(); // skip the last name
+			return in.nextInt();
+		}
+		catch (Exception e) {
+			return -1;
+		}
+		finally {
+			if (in != null) in.close();
+		}
+	}
+
+	public static int getNumber(int line, String file) {
+		Scanner in = null;
+		try {
+			if (line < 1) return -1;
+			in = new Scanner(new BufferedReader(new FileReader(file)));
+			for (int i = 0; i < line - 1; ++i) in.nextLine();
+			in.next(); // skip the first name
+			in.next(); // skip the last name
+			in.nextInt(); // skip the age
+			return in.nextInt();
+		}
+		catch (Exception e) {
+			return -1;
+		}
+		finally {
+			if (in != null) in.close();
+		}
+	}
+
+	public static void fileAppend(String output, String filename) {
+		BufferedWriter bw = null;
+		try {
+			bw = new BufferedWriter(new FileWriter(filename, true));
+			bw.write(output);
+		}
+		catch (IOException e) {
+			return;
+		}
+		finally {
+			try {
+				if (bw != null) bw.close();
+			}
+			catch (IOException e) {
+				return;
+			}
+		}
 	}
 }
